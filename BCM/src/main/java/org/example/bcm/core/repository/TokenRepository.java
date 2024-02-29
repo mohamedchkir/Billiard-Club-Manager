a16c38ad8b9c87b70d7725b4d9ca530a94aaca4f
mohamed chkir <mohamedchkir2@gmail.com>
2024-02-29 16:31:38 +0100

    SECURITY: Create token repository and TokenServiceImpl that contain methods and add DataBaseConstraintException

---
+++ BCM/src/main/java/org/example/bcm/core/repository/TokenRepository.java
@@ -0,0 +1,18 @@
+package org.example.bcm.core.repository;
+
+import org.example.bcm.core.model.entity.Token;
+import org.example.bcm.core.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+@Repository
+public interface TokenRepository extends JpaRepository<Token,Long> {
+    List<Token> findByUser(User user);
+
+    Optional<Token> findByToken(String token);
+
+    Optional<Token> findByUuid(UUID uuid);
+}
