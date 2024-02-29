de87747004cfafee1f99300f1784542b8920f99c
mohamed chkir <mohamedchkir2@gmail.com>
2024-02-29 15:36:54 +0100

    SECURITY: Add App config and create user repo and service

---
+++ BCM/src/main/java/org/example/bcm/core/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package org.example.bcm.core.repository;
+
+import org.example.bcm.core.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+    Optional<User> findByUsername(String username);
+}
---
+++ BCM/src/main/java/org/example/bcm/security/config/AppConfig.java
@@ -0,0 +1,43 @@
+package org.example.bcm.security.config;
+
+import lombok.RequiredArgsConstructor;
+import org.example.bcm.core.service.impl.UserService;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class AppConfig {
+    private final UserService userService;
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+}
+
+
