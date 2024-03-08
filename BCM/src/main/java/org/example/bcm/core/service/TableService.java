a3a77399d84e846ddef7b54e1a2631ec8e712a4d
mohamed chkir <mohamedchkir2@gmail.com>
2024-03-08 22:14:11 +0100

    DEV : Create Crud methods in the tableservice interface and implement it without logic

--- BCM/src/main/java/org/example/bcm/core/service/TableService.java
+++ BCM/src/main/java/org/example/bcm/core/service/TableService.java
@@ -1,4 +1,21 @@
 package org.example.bcm.core.service;
 
+import org.example.bcm.core.model.dto.request.TableRequestDto;
+import org.example.bcm.core.model.dto.request.update.UpdateTableRequestDto;
+import org.example.bcm.core.model.dto.response.TableResponseDto;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public interface TableService {
+    TableResponseDto createTable(TableRequestDto tableRequestDto);
+
+    TableResponseDto getTableById(Long tableId);
+
+    List<TableResponseDto> getAllTables();
+
+    TableResponseDto updateTable(UpdateTableRequestDto updateTableRequestDto);
+
+    void deleteTable(Long tableId);
 }
--- BCM/src/main/java/org/example/bcm/core/service/impl/TableServiceImpl.java
+++ BCM/src/main/java/org/example/bcm/core/service/impl/TableServiceImpl.java
@@ -1,6 +1,35 @@
 package org.example.bcm.core.service.impl;
 
+import org.example.bcm.core.model.dto.request.TableRequestDto;
+import org.example.bcm.core.model.dto.request.update.UpdateTableRequestDto;
+import org.example.bcm.core.model.dto.response.TableResponseDto;
 import org.example.bcm.core.service.TableService;
 
+import java.util.List;
+
 public class TableServiceImpl implements TableService {
+    @Override
+    public TableResponseDto createTable(TableRequestDto tableRequestDto) {
+        return null;
+    }
+
+    @Override
+    public TableResponseDto getTableById(Long tableId) {
+        return null;
+    }
+
+    @Override
+    public List<TableResponseDto> getAllTables() {
+        return null;
+    }
+
+    @Override
+    public TableResponseDto updateTable(UpdateTableRequestDto updateTableRequestDto) {
+        return null;
+    }
+
+    @Override
+    public void deleteTable(Long tableId) {
+
+    }
 }
